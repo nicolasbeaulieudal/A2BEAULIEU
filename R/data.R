cbce06b545d9250295d467d77a55d8148acb4416
Nicolas Beaulieu <nicolas.beaulieu@dal.ca>
2025-02-26 14:34:57 -0400

    Added and documented a copy of the cars dataset.

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Cars Dataset
+#'
+#' This dataset contains speed and stopping distances of cars.
+#'
+#' @format A data frame with 50 observations on 2 variables:
+#' \describe{
+#'   \item{speed}{A numeric vector representing the speed of the car in mph.}
+#'   \item{dist}{A numeric vector representing the stopping distance of the car in feet.}
+#' }
+#' @source The built-in R dataset \code{cars}.
+"cars"
