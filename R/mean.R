85d034b33d001548304aeb0e04b63cb0b14946a9
Nicolas Beaulieu <nicolas.beaulieu@dal.ca>
2025-02-26 14:27:54 -0400

    Added and documented a function to compute a mean.

---
+++ R/mean.R
@@ -0,0 +1,10 @@
+#' Calculate Mean of a Numeric Vector
+#'
+#' This function returns the mean of the given numeric vector.
+#'
+#' @param x A numeric vector for which the mean is computed.
+#' @return A numeric value representing the mean of x.
+#' @export
+mymean <- function(x) {
+  mean(x)
+}
